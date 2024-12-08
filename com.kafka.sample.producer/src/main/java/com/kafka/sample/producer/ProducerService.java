aff3ee2e4beba60b505cdeb5e6e8155abb408367
Griotold <gotjd9773@naver.com>
2024-12-08 17:43:47 +0900

    [consumer] consumer 완료

--- com.kafka.sample.producer/src/main/java/com/kafka/sample/producer/ProducerService.java
+++ com.kafka.sample.producer/src/main/java/com/kafka/sample/producer/ProducerService.java
@@ -1,9 +1,11 @@
 package com.kafka.sample.producer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProducerService {
@@ -13,7 +15,7 @@ public class ProducerService {
 
     public void sendMessage(String topic , String key, String message) {
         for (int i = 0; i < 10; i++) {
-
+            log.info("ProductService.sendMessage topic = {}, key = {}, message = {}", topic, key, message + " " + i);
             kafkaTemplate.send(topic, key, message + " " + i);
         }
 
